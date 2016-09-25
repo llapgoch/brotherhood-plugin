cf47099bf65e3f0701a1c9723fe9bb4bbf904109
David Baker <dave@Davids-MacBook-Pro.local>
2016-09-25 20:21:56 +0100

    Removed session_start

--- brotherhood.php
+++ brotherhood.php
@@ -23,7 +23,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-session_start();
 require_once('inc/includes.php');
 
 DEFINE(DS, DIRECTORY_SEPARATOR);
